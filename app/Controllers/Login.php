e2396327f075272bac9d1fca8c71ef5a21d55ecd
Mohamed-Khater <mohamed.15751@gmail.com>
2022-07-16 15:02:50 +0200

    add mdBootstrap and create login and register

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -8,4 +8,12 @@ class Login extends BaseController {
     public function index(){
 		$this->showView('loginView');
     }
+	
+	public function handleSginin(){
+		
+	}
+	
+	public function handleRegister(){
+		
+	}
 }
