9b7b91386fb1d2e90fe44c97350c7e73e1a90d90
Mohamed-Khater <mohamed.15751@gmail.com>
2022-07-16 20:14:08 +0200

    Create Login and Register

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,6 +36,15 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->add('/', 'Login::index');
+$routes->add('/Login', 'Login::index');
+$routes->add('/login', 'Login::index');
+
+
+$routes->post('handleSginin', 'Login::handleSginin');
+$routes->post('handleRegister', 'Login::handleRegister');
+
+$routes->add('home', 'Home::index');
+
 
 /*
  * --------------------------------------------------------------------
